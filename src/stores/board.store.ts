efaca8c8b67f780cb5a123dcf3d6acf887d65e21
Anwar-Barakat <brkatanwar0@gmail.com>
2025-04-12 21:32:24 +0400

    Implement persistent state management in board store
    - Enhanced the `useBoardStore` to persist state using `zustand/middleware` with localStorage.
    - Updated the store to maintain board slice state across sessions for improved user experience.

--- src/stores/board.store.ts
+++ src/stores/board.store.ts
@@ -1,9 +1,18 @@
 import { create } from "zustand";
 import { createBoardSlice, type BoardSlice } from "./slices/board-slice";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-const useBoardStore = create<BoardSlice>((...args) => ({
-  ...createBoardSlice(...args),
-}));
+const useBoardStore = create<BoardSlice>()(
+  persist(
+    (...args) => ({
+      ...createBoardSlice(...args),
+    }),
+    {
+      name: "board",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export const useBoards = () => useBoardStore((state) => state.boards);
 export const useIsLoading = () => useBoardStore((state) => state.isLoading);
